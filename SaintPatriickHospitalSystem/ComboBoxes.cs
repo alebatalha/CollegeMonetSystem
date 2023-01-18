e814a4760c9daf3a1375594802aac691a0a1d0ed
Alessandra Batalha <98042505+alebatalha@users.noreply.github.com>
2023-01-18 14:02:53 +0000

    Add project files.

---
+++ SaintPatriickHospitalSystem/ComboBoxes.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeMonetSystem
+{
+    //Sets values for county combobox
+    enum Counties
+    {
+        Antrim,
+        Armagh,
+        Carlow,
+        Cavan,
+        Clare,
+        Cork,
+        Derry,
+        Donegal,
+        Down,
+        Dublin,
+        Fermanagh,
+        Galway,
+        Kerry,
+        Kildare,
+        Kilkenny,
+        Laois,
+        Leitrim,
+        Limerick,
+        Longford,
+        Louth,
+        Mayo,
+        Meath,
+        Monaghan,
+        Offaly,
+        Roscommon,
+        Sligo,
+        Tipperary,
+        Tyrone,
+        Waterford,
+        Westmeath,
+        Wexford,
+        Wicklow
+
+    }
+    //Sets values for speacialties combobox
+    enum Specialties
+    {
+    ArchitectureClub,
+    ChemistryClub,
+    EconomicsClub,
+    AstronomyClub,
+    BiologyClub,
+    ElectronicsClub,
+    EngineeringClub,
+    EnglishClub,
+    PoetryClub,
+    LiteraryMagazineClub,
+    AmericanMathematicsCompetitions,
+    ChemistryOlympiad,
+    AmericanRegionsMathLeague,
+    CaribouMathematicsCompetition,
+    WritingClub,
+    FIRSTRoboticsCompetition,
+    HighSchoolInnovationChallenge,
+    NationalSpellingBee,
+    MathLeague,
+    Pottery,
+    Painting,
+    ArtClub,
+    FashionClub,
+    GraphicDesign,
+    Blacksmithing,
+    Carpentering,
+    Sculpture,
+    Sewing,
+    DramaClub,
+    Theater,
+    Photography,
+    JewelryMaking,
+    Danncing,
+    Singing,
+    Swimming,
+    Football,
+    Badminton,
+    RockClimbing,
+    Martial Arts,
+    Kickboxing,
+    Judo,
+    Karate,
+    Skating,
+    Gymnastics,
+    YogaClub,
+    Cycling,
+    Rowing,
+    Racing,
+    Tennis,
+    Volleyball,
+
+    }
+}
---
+++ SaintPatriickHospitalSystem/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeMonetSystem
+{
+    //Inherit the functionality of base class Person
+    class Student : Person
+    {
+        public int StudentNumber { get; set; }
+        public Student(string fn, string sn, string address, string county, int age, string phone, string email, int patNo)
+            : base(fn, sn, address, county, age, phone, email)
+        {
+            StudentNumber = patNo;
+        }
+        //Override ToString() method
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ToString());
+            sb.Append($"Student Number: {StudentNumber}");
+            return sb.ToString();
+        }
+    }
+}
